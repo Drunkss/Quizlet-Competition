b019bfe06e59cc8f87f3ad1ae732fe64321f9844
Avid Eizen <davidessien243@gmail.com>
2023-06-05 15:30:48 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const terms = [
+    { term: 'HTML', definition: 'Hyper Text Markup Language' },
+    { term: 'CSS', definition: 'Cascading Style Sheets' },
+    { term: 'JS', definition: 'JavaScript' },
+    { term: 'DOM', definition: 'Document Object Model' },
+    { term: 'HTTP', definition: 'Hypertext Transfer Protocol' },
+    { term: 'HTTPS', definition: 'Hypertext Transfer Protocol Secure' },
+    { term: 'API', definition: 'Application Programming Interface' },
+    { term: 'JSON', definition: 'JavaScript Object Notation' },
+    { term: 'AJAX', definition: 'Asynchronous JavaScript and XML' },
+    { term: 'XML', definition: 'Extensible Markup Language' },
+    { term: 'XHTML', definition: 'Extensible Hypertext Markup Language' },
+    { term: 'XSLT', definition: 'Extensible Stylesheet Language Transformations' },
+]
